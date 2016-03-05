0aba6ab6b776a6ca68ccdbd033a5b51b94ea347e
Matt Jones <mattjones@yieldbot.com>
2016-03-05 02:51:03 -0500

    update repo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,7 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 
 This CHANGELOG follows the format listed at [Keep A Changelog](http://keepachangelog.com/)
 
-## 
+## [Unreleased]
 
 ## [0.1.0] - 2015-11-06
 ### Fixed
@@ -17,7 +17,6 @@ This CHANGELOG follows the format listed at [Keep A Changelog](http://keepachang
 - updated sensu-plugin gem to 1.2.0
 
 ## [0.0.2] - 2015-06-03
-
 ### Fixed
 - added binstubs
 
@@ -25,7 +24,10 @@ This CHANGELOG follows the format listed at [Keep A Changelog](http://keepachang
 - removed cruft from /lib
 
 ## 0.0.1 - 2015-05-21
-
 ### Added
 - initial release
 
+[Unreleased]: https://github.com/sensu-plugins/sensu-plugins-victorops/compare/0.1.0...HEAD
+[0.1.0]: https://github.com/sensu-plugins/sensu-plugins-victorops/compare/0.0.3...0.1.0
+[0.0.3]: https://github.com/sensu-plugins/sensu-plugins-victorops/compare/0.0.2...0.0.3
+[0.0.2]: https://github.com/sensu-plugins/sensu-plugins-victorops/compare/0.0.1...0.0.2
--- bin/handler-victorops.rb
+++ bin/handler-victorops.rb
@@ -30,10 +30,10 @@ class VictorOps < Sensu::Handler
   def handle
     # validate that we have settings
     unless defined? settings[config[:settingsname]] && !settings[config[:settingsname]].nil?
-      fail "victorops.rb sensu setting '#{config[:settingsname]}' not found or empty"
+      raise "victorops.rb sensu setting '#{config[:settingsname]}' not found or empty"
     end
     unless defined? settings[config[:settingsname]]['api_url'] && !settings[config[:settingsname]]['api_url'].nil?
-      fail "victorops.rb sensu setting '#{config[:settingsname]}.api_url' not found or empty"
+      raise "victorops.rb sensu setting '#{config[:settingsname]}.api_url' not found or empty"
     end
     api_url = settings[config[:settingsname]]['api_url']
 
@@ -42,7 +42,7 @@ class VictorOps < Sensu::Handler
     routing_key = settings[config[:settingsname]]['routing_key'] if routing_key.nil?
 
     unless defined? routing_key && !routing_key.nil?
-      fail 'routing key not defined, should be in Sensu settings or passed via command arguments'
+      raise 'routing key not defined, should be in Sensu settings or passed via command arguments'
     end
 
     incident_key = @event['client']['name'] + '/' + @event['check']['name']
@@ -56,11 +56,11 @@ class VictorOps < Sensu::Handler
       timeout(10) do
         case @event['action']
         when 'create'
-          case @event['check']['status']
-          when 1
-            message_type = 'WARNING'
-          else
-            message_type = 'CRITICAL'
+          message_type = case @event['check']['status']
+                         when 1
+                           'WARNING'
+                         else
+                           'CRITICAL'
           end
         when 'resolve'
           message_type = 'RECOVERY'
